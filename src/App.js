4fe8d7809527b026aa9b46bd2e5709749694d59d
dmitriykoms97 <workkoms97@gmail.com>
2023-07-13 15:33:08 +0300

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// COMPONENTS
+import MainLayout from './components/layouts/MainLayout';
+import SignUp from './components/pages/SignUp';
+
+function App() {
+  return (
+    <MainLayout>
+      <SignUp />
+    </MainLayout>
+  );
+}
+
+export default App;
